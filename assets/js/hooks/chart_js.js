f042cdc0298ef8e4111012267980288053e1e37f
Saul Rojas <saul@getsavi.com>
2023-09-28 12:47:01 -0600

    adds another chart

--- assets/js/hooks/chart_js.js
+++ assets/js/hooks/chart_js.js
@@ -1,43 +1,48 @@
-import Chart from 'chart.js/auto'
+import Chart from "chart.js/auto";
 
 const ChartJS = {
-    dataset() { return JSON.parse(this.el.dataset.chartData).data; },
-    labels() { return JSON.parse(this.el.dataset.chartData).labels; },
-    mounted(){
-      console.log(this.el.dataset.element)
-          const chart = new Chart(
-            document.getElementById(this.el.dataset.element),
-            {
-              type: 'line',
-              data: {
-                labels: this.labels(),
-                datasets: [
-                  {
-                    label: 'Temperature Monitoring',
-                    data: this.dataset()
-                  }
-                ]
-              },
-              options: {
-                animations: {
-                  tension: {
-                    duration: 1000,
-                    easing: 'linear',
-                    from: 1,
-                    to: 0,
-                    loop: true
-                  }
-                },
-              }
-            }
-          );
-          
-        this.handleEvent("update-points", function(payload){ 
-            chart.data.datasets[0].data = payload.data;
-            chart.data.labels = payload.labels;
-            chart.update();
-          })
-    }
-}
- 
+  dataset() {
+    return JSON.parse(this.el.dataset.chartData).data;
+  },
+  labels() {
+    return JSON.parse(this.el.dataset.chartData).labels;
+  },
+  mounted() {
+    const chart = new Chart(document.getElementById(this.el.dataset.element), {
+      type: this.el.dataset.type,
+      data: {
+        labels: this.labels(),
+        datasets: [
+          {
+            label: "Temperature Monitoring",
+            data: this.dataset(),
+          },
+        ],
+      },
+      options: {
+        layout: {
+          padding: {
+            bottom: 1
+          }
+      },
+        animations: {
+          tension: {
+            duration: 1000,
+            easing: "linear",
+            from: 1,
+            to: 0,
+            loop: true,
+          }
+        },
+      },
+    });
+
+    this.handleEvent("update-points", function (payload) {
+      chart.data.datasets[0].data = payload.data;
+      chart.data.labels = payload.labels;
+      chart.update();
+    });
+  },
+};
+
 export default ChartJS;
